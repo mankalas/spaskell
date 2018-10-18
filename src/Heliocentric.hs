31f593bb0335d0a69fab9c3590e5da6a8c11c226
Vincent Boucheny <vincent.boucheny@fluxfederation.com>
2018-10-19 12:06:09 +1300

    Specify max number of terms to prevent catch-all functions

--- src/Heliocentric.hs
+++ src/Heliocentric.hs
@@ -8,31 +8,35 @@ import BTerms
 import RTerms
 import Utilities
 
-type EarthTerm = Int -> (Double, Double, Double)
+type EarthTerm = (Int -> (Double, Double, Double), Int)
 
--- Lx_i
-earthPeriodicTerm :: Double -> (Double, Double, Double) -> Double
-earthPeriodicTerm jme (a, b, c) = a * cos(b + c * jme)
+lTerms_ = [(l0, 64), (l1, 34), (l2, 20), (l3, 7), (l4, 3), (l5, 1)]
+bTerms_ = [(b0, 5), (b1, 2)]
+rTerms_ = [(r0, 40), (r1, 10), (r2, 6), (r3, 2), (r4, 1)]
 
--- Lx
-earthPeriodicTermSummation :: Double -> EarthTerm -> Double
-earthPeriodicTermSummation jme term = sum $ map (earthPeriodicTerm jme) $ map term [0..63]
+earthPeriodicTerm_ :: (Double, Double, Double) -> Double -> Double
+earthPeriodicTerm_ (a, b, c) jme = a * cos(b + c * jme)
 
-earthValues :: [EarthTerm] -> Double -> Double
-earthValues terms jme =
-  (sum $ map earthValue [0..5]) / 1e8
-  where earthValue i =
-          let term = if i < length terms then terms !! i else const (0, 0, 0) in
-            earthPeriodicTermSummation jme term * jme ^^ i
+earthPeriodicTermSummation_ :: EarthTerm -> Double -> Double
+earthPeriodicTermSummation_ (term, max) jme = sum $ map (flip earthPeriodicTerm_ jme) $ map term [0..max - 1]
+
+earthValues_ :: [EarthTerm] -> Double -> Double
+earthValues_ terms jme =
+  (sum $ map earthValue_ [0..5]) / 1e8
+  where earthValue_ i =
+          let term = if i < length terms then terms !! i else (const (0, 0, 0), 1) in
+            earthPeriodicTermSummation_ term jme * jme ^^ i
+
+-- export
 
 -- L
 earthLongitude :: Double -> Double
-earthLongitude = limitDegrees . radianToDegree . earthValues [l0, l1, l2, l3, l4, l5]
+earthLongitude = limitDegrees . radianToDegree . earthValues_ lTerms_
 
 -- B
 earthLatitude :: Double -> Double
-earthLatitude = radianToDegree . earthValues [b0, b1]
+earthLatitude = radianToDegree . earthValues_ bTerms_
 
 -- R
 earthRadiusVector :: Double -> Double
-earthRadiusVector = earthValues [r0, r1, r2, r3, r4]
+earthRadiusVector = earthValues_ rTerms_
