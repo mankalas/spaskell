c0cba11d24bc3deb05e862101473df407c99e004
Vincent Boucheny <vincent.boucheny@fluxfederation.com>
2018-10-19 10:19:27 +1300

    Fix casing of functions' names

--- src/Heliocentric.hs
+++ src/Heliocentric.hs
@@ -1,4 +1,4 @@
--- | 
+-- |
 
 module Heliocentric where
 
@@ -7,20 +7,20 @@ import LTerms
 import Utilities
 
 -- Lx_i
-earth_periodic_term :: GregorianDate -> (Double, Double, Double) -> Double
-earth_periodic_term d terms =
+earthPeriodicTerm :: GregorianDate -> (Double, Double, Double) -> Double
+earthPeriodicTerm d terms =
   let (a, b, c) = terms in
     a * cos(b + c * julianEphemerisMillenium d)
 
 -- Lx
-earth_periodic_term_summation :: GregorianDate -> (Int -> (Double, Double, Double)) -> Double
-earth_periodic_term_summation d l_term = sum $ map (earth_periodic_term d) $ map l_term [0..63]
+earthPeriodicTermSummation :: GregorianDate -> (Int -> (Double, Double, Double)) -> Double
+earthPeriodicTermSummation d l_term = sum $ map (earthPeriodicTerm d) $ map l_term [0..63]
 
 -- L_r
-earth_longitude :: GregorianDate -> Double
-earth_longitude d =
-  limitDegrees $ radianToDegree $ (sum $ map earth_value [0..5]) / 1e8
-  where earth_value i =
+earthLongitude :: GregorianDate -> Double
+earthLongitude d =
+  limitDegrees $ radianToDegree $ (sum $ map earthValue [0..5]) / 1e8
+  where earthValue i =
           let l_terms = [l0, l1, l2, l3, l4, l5]
               jme = julianEphemerisMillenium d in
-            earth_periodic_term_summation d (l_terms !! i) * jme ^^ i
+            earthPeriodicTermSummation d (l_terms !! i) * jme ^^ i
